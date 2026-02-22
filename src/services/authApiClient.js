9e236c6cb251d2c78445dfc4cb0b16f24a5c18a6
Jahid Alam <jahidalam01741@gmail.com>
2026-02-22 01:33:38 -0800

    ready for demployment

--- src/services/authApiClient.js
+++ src/services/authApiClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const authApiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "https://house-rent-backend.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
